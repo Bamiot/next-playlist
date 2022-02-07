ad9ed7ae7d1ffdcc20389c8d0eb23623ee8c1f3e
Louis CORD'HOMME <louis.cordhomme@gmail.com>
2022-02-07 23:46:22 +0100

    .

--- pages/components/searchedSong/index.jsx
+++ pages/components/searchedSong/index.jsx
@@ -7,20 +7,20 @@ export default function SearchedSong({ song }) {
     <div className={styles.container}>
       <figure>
         <Image
-          src={song.thumbnail.url}
-          alt={song.name}
+          src={song ? song.thumbnail.url : ''}
+          alt={song ? song.name : 'error'}
           layout="fixed"
           height="98px"
           width="98px"
         />
       </figure>
       <div>
-        <h1>{song.name}</h1>
-        <span>{song.artist.name}</span>
-        <span>{song.album.name}</span>
-        <span>{`${duration.getMinutes()}:${
-          duration.getSeconds() > 9 ? '' : '0'
-        }${duration.getSeconds()}`}</span>
+        <h1>{song ? song.name : ''}</h1>
+        <span>{song ? song.artist.name : ''}</span>
+        <span>{song ? song.album.name : ''}</span>
+        <span>{`${duration ? duration.getMinutes() : 0}:${
+          duration && duration.getSeconds() > 9 ? '' : '0'
+        }${duration ? duration.getSeconds() : 0}`}</span>
       </div>
     </div>
   ) : null
